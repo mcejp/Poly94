29d48929a11b63551109415cbd2c442db5b33b02
Martin Cejp <cejp.mart@gmail.com>
2022-06-06 17:20:41 +0200

    Improve Video CSR docs

--- rtl/generated/video_inc.sv
+++ rtl/generated/video_inc.sv
@@ -4,6 +4,10 @@ package video_Consts;
   localparam VIDEO_CTRL_FB_EN_OFFSET = 0;
   localparam VIDEO_CTRL_FB_EN = 32'h1;
   localparam ADDR_VIDEO_BG_COLOR = 'h4;
-  localparam VIDEO_BG_COLOR_COLOR_OFFSET = 0;
-  localparam VIDEO_BG_COLOR_COLOR = 32'hffffff;
+  localparam VIDEO_BG_COLOR_R_OFFSET = 16;
+  localparam VIDEO_BG_COLOR_R = 32'hff0000;
+  localparam VIDEO_BG_COLOR_G_OFFSET = 8;
+  localparam VIDEO_BG_COLOR_G = 32'hff00;
+  localparam VIDEO_BG_COLOR_B_OFFSET = 0;
+  localparam VIDEO_BG_COLOR_B = 32'hff;
 endpackage
--- sdk/include/video.h
+++ sdk/include/video.h
@@ -2,20 +2,24 @@
 #define __CHEBY__VIDEO__H__
 #define VIDEO_SIZE 8 /* 0x8 */
 
-/* None */
+/* Control register */
 #define VIDEO_CTRL 0x0UL
 #define VIDEO_CTRL_FB_EN 0x1UL
 
-/* None */
+/* Display background color in RGB888 format */
 #define VIDEO_BG_COLOR 0x4UL
-#define VIDEO_BG_COLOR_COLOR_MASK 0xffffffUL
-#define VIDEO_BG_COLOR_COLOR_SHIFT 0
+#define VIDEO_BG_COLOR_R_MASK 0xff0000UL
+#define VIDEO_BG_COLOR_R_SHIFT 16
+#define VIDEO_BG_COLOR_G_MASK 0xff00UL
+#define VIDEO_BG_COLOR_G_SHIFT 8
+#define VIDEO_BG_COLOR_B_MASK 0xffUL
+#define VIDEO_BG_COLOR_B_SHIFT 0
 
 struct video {
-  /* [0x0]: REG (wo) (no description) */
+  /* [0x0]: REG (wo) Control register */
   uint32_t CTRL;
 
-  /* [0x4]: REG (rw) (no description) */
+  /* [0x4]: REG (rw) Display background color in RGB888 format */
   uint32_t BG_COLOR;
 };
 
